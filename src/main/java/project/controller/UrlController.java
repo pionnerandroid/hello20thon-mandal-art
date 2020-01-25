b24332b87dd98ecfdc444313c7c01f5a60b61fe3
plzprayme <dbfpzk142@gmail.com>
2020-01-25 14:46:24 +0900

    {hello20thon}: 프로젝트 등록

---
+++ src/main/java/project/controller/UrlController.java
@@ -0,0 +1,14 @@
+package project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UrlController {
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
