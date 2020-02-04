925f7f5804f5221d5972ea3e9272457c06a684d4
plzprayme <dbfpzk142@gmail.com>
2020-02-05 04:32:10 +0900

    FRONTEND: bootstrap 파일 추가

--- src/main/java/project/mandalart/service/IdValidation.java
+++
@@ -1,9 +0,0 @@
-package project.mandalart.service;
-
-public class IdValidation {
-    private static final Long NO_ID = 0L;
-
-    public static boolean isNoId(Long id) {
-        return id == NO_ID;
-    }
-}
