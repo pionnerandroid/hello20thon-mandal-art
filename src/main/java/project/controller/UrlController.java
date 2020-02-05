fe91119eb146a923a1970e94c130f6f800213803
plzprayme <dbfpzk142@gmail.com>
2020-02-05 10:58:44 +0900

    FRONT완성, 로그인 기능 삭제

--- src/main/java/project/controller/UrlController.java
+++ src/main/java/project/controller/UrlController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.view.RedirectView;
 import project.mandalart.dto.MandalArtSaveRequestDto;
 import project.mandalart.service.MandalArtService;
 
@@ -14,30 +15,24 @@ public class UrlController {
     private final MandalArtService mandalArtService;
 
     @GetMapping("/")
-//    public String index(Model model, @LoginUser SessionUser user) {
-    public String index() {
-        return "index";
-    }
-
-    @GetMapping("/mandalart")
     public String mandalArt(Model model) {
         Long id = mandalArtService.createNewMandarArt();
         model.addAttribute("mandalart", mandalArtService.findMandalArtById(id));
         return "mandalart/mandalart";
     }
 
-    @GetMapping("/mandalart/{userId}/{mandalId}")
-    public String mandalArt(@PathVariable("userId") Long userId,
-                            @PathVariable("mandalId") Long mandalId, Model model) {
+    @GetMapping("/{mandalId}")
+    public String mandalArt(@PathVariable("mandalId") Long mandalId, Model model) {
         model.addAttribute("mandalart", mandalArtService.findMandalArtById(mandalId));
         return "mandalart/mandalart";
     }
 
-    @PostMapping("/mandalart")
-    public String savemandalArt(@ModelAttribute MandalArtSaveRequestDto requestDto,
-                                Model model) {
+    @PostMapping("/mandalart/save")
+    public RedirectView savemandalArt(@ModelAttribute MandalArtSaveRequestDto requestDto,
+                                      Model model) {
         mandalArtService.update(requestDto.getId(), requestDto);
         model.addAttribute("mandalart", mandalArtService.findMandalArtById(requestDto.getId()));
-        return "mandalart/mandalart";
+        String url = "/" + requestDto.getId();
+        return new RedirectView(url);
     }
 }
